756fa8d913e9b5d091fed3d1e102015638e10726
jchen790 <jchen790@gmail.com>
2018-02-12 07:29:21 -0500

    added a view to test poc

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// using a web page and saving as a file locally for now
+// TODO - switch to an API call and store data in a database
+const fs = require('fs');
+const pug = require('pug');
+app.set('view engine', 'pug');
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+app.get('/', (request, response) => {
+    response.render('index');
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
