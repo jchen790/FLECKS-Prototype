3ff4d1bdde2e42de4801517183caf28defb15c5c
jchen790 <jchen790@gmail.com>
2018-02-12 11:31:42 -0500

    fixed overwrite issue

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let counter = 0;
 const fs = require('fs');
 const wav = require('wav');
 const pug = require('pug');
+const WebSocket = require('ws');
 const BinaryServer = require('binaryjs').BinaryServer;
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/scripts'));
@@ -25,7 +26,7 @@ server = BinaryServer({port: 9001});
 server.on('connection', (client) => {
     console.log('Connection established with client!');
 
-    let filename = 'audio-' + counter + '.wav';
+    let filename = './audio-recordings/audio-' + counter + '.wav';
     let fileWriter = new wav.FileWriter(filename, {
         channels: 1,
         sampleRate: 48000,
@@ -39,6 +40,7 @@ server.on('connection', (client) => {
         stream.on('end', () => {
             fileWriter.end();
             console.log("Stream ended. Audio saved in file " + filename);
+            counter++;
         });
 
         client.on('close', () => {
--- scripts/script.js
+++ scripts/script.js
@@ -5,13 +5,22 @@ client.on('open', () => {
 
     // get access to (browser) mic
     if (!navigator.getUserMedia) {
-        alert('Error in requesting mic access');
+        navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia || navigator.msGetUserMedia;
     }
-    else {
-        var session = { audio: true };
+
+    if (navigator.getUserMedia) {
+        var session = { 
+            audio: true,
+            video: false
+         };
         navigator.getUserMedia(session, audioRec, (err) => {
             alert('Error in capturing audio');
         });
+    }
+    else
+    {
+        alert('Error in requesting mic access');
     }
 });
 
@@ -19,32 +28,37 @@ var isRecording = false;
 
 function startAudioRec() {
     // start recording audio
+    // window.Stream = client.createStream();
     isRecording = true;
+    console.log("Start recording.");
 }
 
 function stopAudioRec() {
     // stop recording audio
     isRecording = false;
+    console.log("Stop recording.");
     window.Stream.end();
 }
 
-function audioRec(e) {
+function audioRec(stream) {
     // send audio through processing nodes
-    let audioContext = window.AudioContext;
+    console.log("Sending audio through processing nodes!");
+    let audioContext = window.AudioContext || window.webkitAudioContext;
     let context = new audioContext();
-    let audioInput = context.createMediaStreamSource(e);
+    let audioInput = context.createMediaStreamSource(stream);
     let bufferSize = 2048;
 
     let recorder = context.createScriptProcessor(bufferSize, 1, 1);
 
     recorder.onaudioprocess = (e) => {
         if (!isRecording) {
+            console.log("--->Not recording");
             return;
         }
-        else {
-            let left = e.inputBuffer.getChannelData(0);
-            window.Stream.write(floatToInt(left));
-        }
+
+        console.log("--->Is recording");
+        let left = e.inputBuffer.getChannelData(0);
+        window.Stream.write(floatToInt(left));
     }
 
     audioInput.connect(recorder);
@@ -53,11 +67,11 @@ function audioRec(e) {
 
 function floatToInt(x) {
     let length = x.length;
-    let buffer = new Int16Array(length);
+    let b = new Int16Array(length);
 
     while (length--) {
-        buffer[length] = buffer[length] * 0xFFFF;
+        b[length] = b[length] * 0xFFFF;
     }
 
-    return buffer.buffer;
+    return b.buffer;
 }
