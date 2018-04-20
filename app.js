5bcaf3368abeaceded7d17b9db6e3b387dfeaafc
jchen790 <jchen790@gmail.com>
2018-04-19 21:31:19 -0400

    audio working!

--- app.js
+++ app.js
@@ -4,26 +4,54 @@ const path = require("path");
 const fs = require("fs");
 const io = require('socket.io')(http);
 
+// Choose which local port
 const port = 3000;
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+// Serve html file
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/script.js', function(req, res){
+// Serve custom js
+app.get('/script.js', function (req, res) {
     res.sendFile(__dirname + '/script.js');
-  });
-
-io.sockets.on('connection', function(socket){
-  console.log('> a user connected');
-
-  socket.on('new_user', function(username) {
-      socket.username = username;
-      socket.emit('user_acked', 'You are connected!');
-      console.log('>>> ' + socket.username + ' has connected');
-  })
 });
 
-http.listen(port, function(){
-  console.log('listening on http://localhost:' + port);
+// Serve recording js
+app.get('/recordrtc/RecordRTC.js', function (req, res) {
+    res.sendFile(__dirname + '/recordrtc/RecordRTC.js');
 });
+
+io.sockets.on('connection', function (socket) {
+    console.log('> a user connected');
+
+    // New user connected - log user
+    socket.on('new_user', function (username) {
+        socket.username = username;
+        socket.emit('user_acked', 'You are connected!');
+        console.log('>>> ' + socket.username + ' has connected');
+    });
+
+    // Collect audio stream
+    socket.on('audio', function (data) {
+        let fileName = socket.username + '-' + Date.now();
+
+        writeAudioFile(data.audio.dataURL, fileName + '.wav');
+    });
+});
+
+http.listen(port, function () {
+    console.log('listening on http://localhost:' + port);
+});
+
+function writeAudioFile(dataURL, fileName)
+{
+    let filePath = './audio-recordings/' + fileName;
+    let fileID = 2;
+    let fileBuffer;
+    dataURL = dataURL.split(',').pop();
+    fileBuffer = new Buffer(dataURL, 'base64');
+    fs.writeFileSync(filePath, fileBuffer);
+
+    console.log('> audio file saved as ' + filePath);
+}
--- script.js
+++ script.js
@@ -1,9 +1,62 @@
+$('#start-recording').prop("disabled", true);
+$('#stop-recording').prop("disabled", true);
+
 var socketio = io();
 
 var username = prompt('username?');
+
+var mediaStream = null;
 
 socketio.emit('new_user', username);
 
 socketio.on('user_acked', function (message) {
     $('#server-message').text(message);
-})
+    $('#start-recording').prop("disabled", false);
+});
+
+var recordAudio;
+
+$('#start-recording').click(function () {
+    $('#start-recording').prop("disabled", true);
+    navigator.getUserMedia({
+        audio: true
+    }, function (stream) {
+        mediaStream = stream;
+
+        recordAudio = RecordRTC(stream, {
+            type: 'audio', 
+            recorderType: StereoAudioRecorder,
+            onAudioProcessStarted: function() {
+
+            }
+        });
+
+        recordAudio.startRecording();
+
+        $('#stop-recording').prop("disabled", false);
+    }, function(error) {
+        alert('Recording error - ' + JSON.stringify(error));
+    });
+});
+
+$('#stop-recording').click(function() {
+    $('#start-recording').prop("disabled", false);
+    $('#stop-recording').prop("disabled", true);
+
+    recordAudio.stopRecording(function() {
+        recordAudio.getDataURL(function(audioDataURL) {
+            var files = {
+                audio: {
+                    type: recordAudio.getBlob().type || 'audio/wav',
+                    dataURL: audioDataURL
+                }
+            };
+
+            socketio.emit('audio', files);
+            if (mediaStream)
+            {
+                mediaStream.stop();
+            }
+        });
+    });
+});
